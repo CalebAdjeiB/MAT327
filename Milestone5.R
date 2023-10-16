d3be0204b3b6bd8adedcd9ed895d74a1ff3919e1
CalebAdjeiB <145230882+CalebAdjeiB@users.noreply.github.com>
2023-10-15 23:15:43 -0400

    Add files via upload
    R code for Measure of Center & Spread

---
+++ Milestone5.R
@@ -0,0 +1,30 @@
+#Measure of Center & Spread
+View(heartfail)
+
+#age column
+mean(heartfail$age)
+
+median(heartfail$age)
+
+var(heartfail$age)
+
+sd(heartfail$age)
+
+#ejection fraction column
+mean(heartfail$ejection_fraction)
+
+median(heartfail$ejection_fraction)
+
+var(heartfail$ejection_fraction)
+
+sd(heartfail$ejection_fraction)
+
+#serum sodium column
+mean(heartfail$serum_sodium)
+
+median(heartfail$serum_sodium)
+
+var(heartfail$serum_sodium)
+
+sd(heartfail$serum_sodium)
+
