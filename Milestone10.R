33c7b2b23cd2cc173bd5024630b3265c8b531ba4
CalebAdjeiB <145230882+CalebAdjeiB@users.noreply.github.com>
2023-12-03 22:45:00 -0500

    Add files via upload

---
+++ Milestone10.R
@@ -0,0 +1,7 @@
+#Measure of Center & Spread
+
+#creatinine phosphokinase
+mean(heartfail$creatinine_phosphokinase)
+median(heartfail$creatinine_phosphokinase)
+var(heartfail$creatinine_phosphokinase)
+sd(heartfail$creatinine_phosphokinase)
